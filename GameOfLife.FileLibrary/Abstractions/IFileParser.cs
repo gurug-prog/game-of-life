7e1a3fc55be254087acbfec9bb1dcdb881a8afb6
gurug-prog <ivanmysterys@gmail.com>
2022-10-30 19:02:29 +0200

    feat: add file library skeleton

---
+++ GameOfLife.FileLibrary/Abstractions/IFileParser.cs
@@ -0,0 +1,27 @@
+﻿using GameOfLife.Model;
+
+namespace GameOfLife.FileLibrary;
+
+public interface IFileParser<TGeneration, TCell>
+    where TGeneration : IGeneration<TCell>
+{
+    // 
+    // Summary:
+    //     Parses the info contained in the fileReader.
+    void Parse(IFileReader fileReader);
+    //
+    // Summary:
+    //     Count of generations to be calculated.
+    //
+    // Returns:
+    //     Integer value of generations.
+    int Count { get; }
+    //
+    // Summary:
+    //     The generation build from the file.
+    //
+    // Returns:
+    //     The generation to which the calculations
+    //     may be applied.
+    TGeneration Generation { get; }
+}
---
+++ GameOfLife.FileLibrary/Abstractions/IFileWriter.cs
@@ -0,0 +1,12 @@
+﻿using GameOfLife.Model;
+
+namespace GameOfLife.FileLibrary;
+
+public interface IFileWriter<TGeneration, TCell>
+    where TGeneration : IGeneration<TCell>
+{
+    //
+    // Summary:
+    //     Writes the generation to a specified file path.
+    void Write(string path, TGeneration generation);
+}
