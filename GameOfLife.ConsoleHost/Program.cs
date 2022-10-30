635f0473589fe44a5ef9f204aefed5f18f4d4771
gurug-prog <ivanmysterys@gmail.com>
2022-10-30 20:32:55 +0200

    feat: implement draft of usage

---
+++ GameOfLife.ConsoleHost/Program.cs
@@ -0,0 +1,39 @@
+﻿using GameOfLife.Model;
+using GameOfLife.FileLibrary;
+
+namespace GameOfLife.ConsoleHost;
+public class Program
+{
+    public static void Main()
+    {
+        const string INPUT_FILE_PATH = "./input.txt";
+        const string OUTPUT_FILE_PATH = "./output.txt";
+        var fileReader = new ClassicFileReader();
+        fileReader.Read(INPUT_FILE_PATH);
+
+        var fileParser = new ClassicFileParser();
+        fileParser.Parse(fileReader);
+
+        // must be configurated with
+        // fileParser.Generation
+        // and rules
+        var universe = new ClassicUniverse();
+        var iteration = 0;
+
+        foreach (var generation in universe.Take(fileParser.Count))
+        {
+            Console.WriteLine($"Generation: {iteration++}");
+            Console.WriteLine(generation);
+            Console.WriteLine();
+
+            if (iteration == fileParser.Count)
+            {
+                var fileWriter = new ClassicFileWriter();
+                fileWriter.Write(OUTPUT_FILE_PATH, generation);
+            }
+        }
+
+        Console.WriteLine("Written generation:");
+        Console.WriteLine(fileParser.Generation);
+    }
+}
