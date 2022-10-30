de0930e3766c17ff75bd7f8f89c30c5ba3e1fb47
gurug-prog <ivanmysterys@gmail.com>
2022-10-30 19:25:34 +0200

    feat: add file library implementation stubs

---
+++ GameOfLife.FileLibrary/Implementations/ClassicFileReader.cs
@@ -0,0 +1,15 @@
+﻿namespace GameOfLife.FileLibrary;
+
+public sealed class ClassicFileReader : IFileReader
+{
+    public string CountString => throw new NotImplementedException();
+
+    public string GenerationHeader => throw new NotImplementedException();
+
+    public string GenerationString => throw new NotImplementedException();
+
+    public void Read(string path)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ GameOfLife.FileLibrary/Implementations/ClassicFileWriter.cs
@@ -0,0 +1,11 @@
+﻿using GameOfLife.Model;
+
+namespace GameOfLife.FileLibrary;
+
+public sealed class ClassicFileWriter : IFileWriter<ClassicGeneration, ClassicCell>
+{
+    public void Write(string path, ClassicGeneration generation)
+    {
+        throw new NotImplementedException();
+    }
+}
