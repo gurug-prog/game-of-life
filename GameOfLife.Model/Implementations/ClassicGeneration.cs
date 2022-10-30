b922b8c3a5365c1f49989e5154385344ed95a748
gurug-prog <ivanmysterys@gmail.com>
2022-10-30 19:25:07 +0200

    feat: add model implementation stubs

---
+++ GameOfLife.Model/Implementations/ClassicGeneration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace GameOfLife.Model;
+
+public sealed class ClassicGeneration : IGeneration<ClassicCell>
+{
+    public int Columns => throw new NotImplementedException();
+
+    public int Rows => throw new NotImplementedException();
+
+    public IEnumerator<ClassicCell> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ GameOfLife.Model/Implementations/ClassicUniverse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+namespace GameOfLife.Model;
+
+public sealed class ClassicUniverse : IUniverse<
+    ClassicGeneration,
+    ClassicGameRules,
+    ClassicCell>
+{
+    public ClassicGeneration Initial
+    {
+        get { throw new NotImplementedException(); }
+    }
+
+    public ClassicGameRules Rules
+    {
+        get { throw new NotImplementedException(); }
+    }
+
+    public IEnumerator<ClassicGeneration> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
