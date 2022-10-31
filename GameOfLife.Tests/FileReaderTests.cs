2b41c1962568b7d659d0d4f9c30b8f0a42420e0e
gurug-prog <ivanmysterys@gmail.com>
2022-10-31 18:38:48 +0200

    test: add file reader tests

--- GameOfLife.Tests/FileReaderTests.cs
+++ GameOfLife.Tests/FileReaderTests.cs
@@ -1,10 +1,55 @@
-﻿namespace GameOfLife.Tests;
+﻿using GameOfLife.FileLibrary;
+
+namespace GameOfLife.Tests;
 
 public class FileReaderTests
 {
     [Fact]
-    public void Test1()
+    public void Read_EmptyFilepathIsSpecified_Throws_IOException()
     {
+        const string path = "";
+        IFileReader fileReader = new ClassicFileReader();
+
+        Assert.Throws<IOException>(() => fileReader.Read(path));
+    }
+
+    [Fact]
+    public void Read_BadFilepathIsSpecified_Throws_IOException()
+    {
+        const string path = "what !the f@$$ is it [a really --correc*t path???";
+        IFileReader fileReader = new ClassicFileReader();
+
+        Assert.Throws<IOException>(() => fileReader.Read(path));
+    }
+
+    [Fact]
+    public void Read_FileHasNoRequiredInfo_Throws_FormatException()
+    {
+        const string path = "../Assets/test-file_no-required-info.txt";
+        IFileReader fileReader = new ClassicFileReader();
+
+        Assert.Throws<FormatException>(() => fileReader.Read(path));
+    }
+
+    [Fact]
+    public void Read_FileIsEmpty_Throws_FormatException()
+    {
+        const string path = "../Assets/empty-file.txt";
+        IFileReader fileReader = new ClassicFileReader();
+
+        Assert.Throws<IOException>(() => fileReader.Read(path));
+    }
+
+    [Fact]
+    public void Read_CorrectFilePathIsSpecified_Returns_NonEmptyValues()
+    {
+        const string path = "../Assets/glider.txt";
+        IFileReader fileReader = new ClassicFileReader();
+
+        fileReader.Read(path);
+        
+        Assert.False(String.IsNullOrEmpty(fileReader.CountString));
+        Assert.False(String.IsNullOrEmpty(fileReader.GenerationHeader));
+        Assert.False(String.IsNullOrEmpty(fileReader.GenerationString));
     }
 }
-
