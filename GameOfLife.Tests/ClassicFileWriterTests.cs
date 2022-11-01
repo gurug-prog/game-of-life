f9ded0f2062487e07d531e6ea49332367c4be16a
gurug-prog <ivanmysterys@gmail.com>
2022-11-01 17:08:54 +0200

    test: add classic file writer tests

--- GameOfLife.Tests/ClassicFileWriterTests.cs
+++ GameOfLife.Tests/ClassicFileWriterTests.cs
@@ -1,9 +1,31 @@
-﻿namespace GameOfLife.Tests;
+﻿using GameOfLife.FileLibrary;
+using GameOfLife.Model;
+using GameOfLife.Model.Implementations;
+
+namespace GameOfLife.Tests;
 
 public class ClassicFileWriterTests
 {
     [Fact]
-    public void Test1()
+    public void Write_FileInCurrentDirectory_Result_FileWasWritten()
     {
+        const string FILE_PATH = "./saved-file.txt";
+        IFileWriter<ClassicGeneration, ClassicCell> writer =
+            new ClassicFileWriter();
+
+        writer.Write(FILE_PATH, ClassicSeed.Blicker);
+
+        Assert.True(File.Exists(FILE_PATH));
+    }
+
+    [Fact]
+    public void Write_PathDoesNotExist_Throws_IOException()
+    {
+        const string FILE_PATH = "hey, what r u finding here???";
+        IFileWriter<ClassicGeneration, ClassicCell> writer =
+            new ClassicFileWriter();
+
+        Assert.Throws<IOException>(
+            () => writer.Write(FILE_PATH, ClassicSeed.Block));
     }
 }
